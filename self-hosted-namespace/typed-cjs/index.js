ff500818487b8576f5e474bd1ed9dcc67f789f90
Pelle Wessman <pelle@kodfabrik.se>
2020-10-16 18:40:37 +0200

    Show that one can refer exported types

--- self-hosted-namespace/typed-cjs/index.js
+++ self-hosted-namespace/typed-cjs/index.js
@@ -3,7 +3,10 @@ const helloWorld = require("../");
 // const { helloWorld: namedHelloWorld } = require("../");
 // const { default: defaultHelloWorld } = require("../");
 
-if (helloWorld() !== 'Hello World') throw new Error('Oh no!');
+/** @type {import('../').HelloWorldResponse} */
+const desiredResult = 'Hello World';
+
+if (helloWorld() !== desiredResult) throw new Error('Oh no!');
 // Non-supported
 // if (namedHelloWorld() !== 'Hello World') throw new Error('Oh no!');
 // if (defaultHelloWorld() !== 'Hello World') throw new Error('Oh no!');
